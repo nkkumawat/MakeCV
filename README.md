562cadeadc0d27a11845362c2e748c452641540f
nkkumawat <nk0kumawat@gmail.com>
2017-10-03 11:48:32 +0530

    final template added

--- README.md
+++
@@ -1,2 +0,0 @@
-# MakeCV
-Make your CV in 2 min.
--- routes/pdf.js
+++ routes/pdf.js
@@ -12,13 +12,27 @@ router.post('/', function(req, res) {
     const Link = req.body.link;
     const CompanyName = req.body.companyname;
     const ExprienceDesc = req.body.descriptionexperience;
+    const InstituteName = req.body.institutename;
+    const EducationDetails = req.body.edudesc;
     // const Education
     const ProjectName = req.body.projectname;
     const ProjectDesc = req.body.descriptionproject;
     const Skills = req.body.skill;
     const Languages = req.body.lang;
+    const Respo = req.body.resp;
 
-    // console.log("Narendra Kumawat" + arr.length);
+    var fs = require('fs');
+    var stream = fs.createWriteStream("public/texts/"+Name +".txt");
+    stream.once('open', function(fd) {
+        stream.write(Name +"\n");
+        stream.write(Website +"\n");
+        stream.write(Mobile +"\n");
+        stream.write(Email +"\n");
+        stream.write(Address +"\n");
+        stream.end();
+    });
+
+
     let filename = Name;
     filename = encodeURIComponent(filename) + '.pdf'; // Stripping special character
     res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"');// Setting response to 'attachment' (download).
@@ -32,10 +46,14 @@ router.post('/', function(req, res) {
     doc.fontSize(10);
     doc.fillColor('#000000');
     doc.font('public/fonts/timeburnernormal.ttf');
-    for(i = 0; i < Skills.length ; i ++) {
-        if(Skills[i] !== '') {
-            doc.text(Skills[i] + "​\n\n", 430);
+    if(Array.isArray(Skills)) {
+        for (i = 0; i < Skills.length; i++) {
+            if (Skills[i] !== '') {
+                doc.text(Skills[i] + "​\n\n", 430);
+            }
         }
+    }else {
+        doc.text(Skills + "​\n\n", 430);
     }
     doc.fontSize(15);
     doc.fillColor('#33ccff');
@@ -44,20 +62,30 @@ router.post('/', function(req, res) {
     doc.fontSize(10);
     doc.fillColor('#000000');
     doc.font('public/fonts/timeburnernormal.ttf');
-    for(i = 0; i < Languages.length ; i ++) {
-        if(Languages[i] !== '') {
-            doc.text( Languages[i] + "​\n\n", 430);
+    if(Array.isArray(Respo)) {
+        for (i = 0; i < Respo.length; i++) {
+            if (Respo[i] !== '') {
+                doc.text(Respo[i] + "​\n\n", 430);
+            }
         }
+    }else {
+        doc.text(Respo + "​\n\n", 430);
     }
     doc.fontSize(15);
     doc.fillColor('#33ccff');
     doc.font('public/fonts/timeburnerbold.ttf');
+    doc.text("\nLanguage\n\n" , 430 );
     doc.fontSize(10);
     doc.fillColor('#000000');
     doc.font('public/fonts/timeburnernormal.ttf');
-    doc.text("\nLanguage\n\n" , 430 );
-    for(i = 0; i < 3 ; i ++) {
-        doc.text("Hindi , English​\n\n", 430);
+    if(Array.isArray(Languages)) {
+        for (i = 0; i < Languages.length; i++) {
+            if (Languages[i] !== '') {
+                doc.text(Languages[i] + "​\n\n", 430);
+            }
+        }
+    }else {
+        doc.text(Languages + "​\n\n", 430);
     }
     doc.fontSize(18);//  font size
     doc.font('public/fonts/timeburnernormal.ttf');
@@ -69,7 +97,8 @@ router.post('/', function(req, res) {
     doc.fillColor('#33ccff');
     doc.text(Link + " | " + Website + " | " + Email , 50 , 65);
     //Address of user
-    doc.fontSize(10);
+    console.log(Address);
+    doc.fontSize(8);
     doc.fillColor('#000000');
     doc.text(Address + "\n" + Mobile + "\n" + Email, 433 , 35);
 
@@ -78,45 +107,77 @@ router.post('/', function(req, res) {
     doc.fillColor('#33ccff');
     doc.font('public/fonts/timeburnerbold.ttf');
     doc.text("Experience\n\n" , 50 , 110);
-    for(i = 0; i < CompanyName.length ; i ++) {
+    if(Array.isArray(CompanyName)) {
+        for (i = 0; i < CompanyName.length; i++) {
+            doc.fontSize(12);
+            doc.bold = 10;
+            doc.fillColor('#000000');
+            doc.font('public/fonts/timeburnerbold.ttf');
+            doc.text(CompanyName[i], 50);
+            doc.fontSize(10);
+            doc.font('public/fonts/timeburnernormal.ttf');
+            doc.text(ExprienceDesc[i] + "\n\n");
+        }
+    }else {
         doc.fontSize(12);
         doc.bold = 10;
         doc.fillColor('#000000');
         doc.font('public/fonts/timeburnerbold.ttf');
-        doc.text(CompanyName[i] , 50 );
+        doc.text(CompanyName, 50);
         doc.fontSize(10);
         doc.font('public/fonts/timeburnernormal.ttf');
-        doc.text(ExprienceDesc[i]+"\n\n" );
+        doc.text(ExprienceDesc + "\n\n");
     }
     doc.fontSize(15);
     doc.fillColor('#33ccff');
     doc.font('public/fonts/timeburnerbold.ttf');
     doc.text("\nEducation\n\n" , 50);
-    for(i = 0; i < 2 ; i ++) {
+
+    if(Array.isArray(InstituteName)) {
+        for (i = 0; i < InstituteName.length; i++) {
+            doc.fontSize(12);
+            doc.bold = 10;
+            doc.fillColor('#000000');
+            doc.font('public/fonts/timeburnerbold.ttf');
+            doc.text(InstituteName[i], 50);
+            doc.fontSize(10);
+            doc.font('public/fonts/timeburnernormal.ttf');
+            doc.text(EducationDetails[i] + "\n\n");
+        }
+    }else {
         doc.fontSize(12);
         doc.bold = 10;
         doc.fillColor('#000000');
         doc.font('public/fonts/timeburnerbold.ttf');
-        doc.text("National Institute of Technology , Kurukshetra" , 50  );
+        doc.text(InstituteName, 50);
         doc.fontSize(10);
         doc.font('public/fonts/timeburnernormal.ttf');
-        doc.text("B.tech Computer Engineering" );
-        doc.fontSize(8);
-        doc.text("CGPA 9.066 \n\n" );
+        doc.text(EducationDetails + "\n\n");
     }
     doc.fontSize(15);
     doc.fillColor('#33ccff');
     doc.font('public/fonts/timeburnerbold.ttf');
     doc.text("\nProjects\n\n" , 50);
-    for(i = 0; i < ProjectName.length ; i ++) {
+    if(Array.isArray(ProjectName)) {
+        for (i = 0; i < ProjectName.length; i++) {
+            doc.fontSize(12);
+            doc.bold = 10;
+            doc.fillColor('#000000');
+            doc.font('public/fonts/timeburnerbold.ttf');
+            doc.text(ProjectName[i], 50);
+            doc.fontSize(8);
+            doc.font('public/fonts/timeburnernormal.ttf');
+            doc.text(ProjectDesc[i] + "\n\n ");
+        }
+    }else {
         doc.fontSize(12);
         doc.bold = 10;
         doc.fillColor('#000000');
         doc.font('public/fonts/timeburnerbold.ttf');
-        doc.text(ProjectName[i] , 50 );
+        doc.text(ProjectName, 50);
         doc.fontSize(8);
         doc.font('public/fonts/timeburnernormal.ttf');
-        doc.text(ProjectDesc[i]+"\n\n " );
+        doc.text(ProjectDesc + "\n\n ");
     }
 
     doc.pipe(res);
